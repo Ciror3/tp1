17227bcc4d983b38581b12cc157b4e6b30226718
Matías Cobos <maticobos2003@gmail.com>
2023-03-22 21:24:16 -0300

    Testing Playlist

--- FileSystem.hs
+++ FileSystem.hs
@@ -7,12 +7,23 @@ data FileSystem = FS [Etiqueta] [Tema] deriving (Eq, Show)
 nuevoF :: FileSystem
 nuevoF = FS [] []
 --Crea un nuevo FileSystem con sus listas vacıas.
+testnuevoF = [nuevoF == FS [] []] --True
+
 etiquetasF :: FileSystem -> [ Etiqueta ]
 etiquetasF (FS eti _) = eti
-temasF :: FileSystem ->[ Tema ]
+testetiquetasF = [etiquetasF (FS ["eti1","eti2"] []) == ["eti1","eti2"], --True
+                  etiquetasF (FS ["eti1","eti2"] []) == ["eti1"]]        --False
+                  
+
+temasF :: FileSystem -> [ Tema ]
 temasF (FS _ tem) = tem
+testtemasF = []
+
 agregarF :: Tema -> Etiqueta ->FileSystem ->FileSystem
 agregarF newtema neweti (FS eti tem) = FS (eti++[neweti]) (insertar newtema tem)
 -- --Agrega el tema y sus etiquetas de ser necesario.
+testagregarF = []
+
 filtrarF :: Etiqueta ->FileSystem ->[ Tema ]
 filtrarF neweti (FS eti tem) = [x | x <- tem, aplicaT neweti x]
+testfiltrarF = []
--- Playlist.hs
+++ Playlist.hs
@@ -12,20 +12,31 @@ nuevaP = Play 0
 
 intP :: Playlist -> Int
 intP (Play n _) = n
+testintP = [intP (Play 0 []) == 0,                       --True
+            intP (Play 0 [nuevoT "temas" "ahora"]) == 0, --True
+            intP (Play 1 [nuevoT "temas" "ahora"]) == 1] --True
 
 actualP :: Playlist -> Tema
 actualP (Play n tema) = tema !! n
 
 skipP :: Playlist -> Playlist
-skipP (Play n tema) |  n < 0 = error "Indice negativo"
+skipP (Play n tema) |  n < 0 = error "Indice negativo" 
                     | otherwise = Play (n+1) tema
+testskipP = [skipP (Play 0 [nuevoT "temas" "ahora"]) == Play 1 [nuevoT "temas" "ahora"], --True
+             skipP (Play 2 [nuevoT "temas" "ahora"]) == Play 3 [nuevoT "temas" "ahora"], --True
+             skipP (Play 2 [nuevoT "temas" "ahora"]) == Play 1 [nuevoT "temas" "ahora"]] --False
 
 backP :: Playlist -> Playlist
 backP (Play n tema) | n < 1 = error "Indice negativo"
                     | otherwise = Play (n-1) tema
 
+testbackP = [backP (Play 1 [nuevoT "temas" "ahora"]) == Play 0 [nuevoT "temas" "ahora"], --True
+             backP (Play 2 [nuevoT "temas" "ahora"]) == Play 1 [nuevoT "temas" "ahora"], --True
+             backP (Play 2 [nuevoT "temas" "ahora"]) == Play 3 [nuevoT "temas" "ahora"]] --False
+
 resetP :: Playlist -> Playlist
 resetP (Play n temas) = Play 0 temas
+testresetP = [resetP (Play 3 [nuevoT "temas" "ahora"]) == Play 0 [nuevoT "temas" "ahora"]]
 
 testnuevaP =[nuevaP [] == Play 0 [],--True
              nuevaP [nuevoT "temas" "ahora"] == Play 0 [nuevoT "temas" "ahora"],--True
